d4d6acb27064be309dab9c3056f2a04c92af31cc
Christopher Jewell <christopherjewell@Christophers-MacBook-Pro.local>
2017-07-18 13:53:35 -0500

    other files

---
+++ turtles.py
@@ -0,0 +1,13 @@
+import turtle
+
+wn = turtle.Screen()             # Set up the window and its attributes
+wn.bgcolor("lightgreen")
+
+tess = turtle.Turtle()           # create tess and set some attributes
+tess.color("hotpink")
+tess.pensize(5)
+
+alex = turtle.Turtle()           # create alex
+
+
+wn.exitonclick()

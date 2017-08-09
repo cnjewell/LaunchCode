0043a6e6615c131c0df6618169f0213d21c32281
Christopher Jewell <christophernjewell@gmail.com>
2017-08-09 13:15:22 -0500

    data_types challenge

---
+++ data_types.py
@@ -0,0 +1,22 @@
+# Data Types
+
+# Linked Lists
+# HEAD...
+
+# collections.namedtuple()
+# class generator
+
+# ring buffers
+# thread safe ring buffers
+
+
+
+
+
+# ASSIGNMENT #
+# insert (let's do end, I guess)
+# delete by arbitrary index (ie pull the 10th object deep)
+# delete by arbitrary value (ie pull the first object that matches the search)
+
+# NEXT # 
+# Binary Tree

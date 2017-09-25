9523724e75cf34265bad27d2e499cb94be5da203
Christopher Jewell <christophernjewell@gmail.com>
2017-09-25 15:52:27 -0500

    Included bulma css framework, spruced up assignment

--- Unit_02/assignment/main.py
+++ Unit_02/assignment/main.py
@@ -17,7 +17,7 @@ login_needed = ['newpost', 'logout']
 @app.before_request
 def require_login():
     if ('user' not in session and request.endpoint in login_needed):
-        flash('Login required to access certain pages.', category='error')
+        flash('Login required to access certain pages.', category="is-warning")
         return redirect("/login")
 
 def logged_in_user():
@@ -53,14 +53,14 @@ def login():
         # VAILDATION #
 
         if email == '' or password == '':
-            flash('Error: One or more fields left blank', category="error")
-            return redirect('/register')
+            flash('Error: One or more fields left blank', category="is-warning")
+            return redirect('/login')
 
         # TODO : Test this works properly, I'm skeptical
 
         users = User.query.filter_by(email=email)
         if users.count() != 1:
-            flash('Error: Email not in our system.', category="error")
+            flash('Error: Email not in our system.', category="is-warning")
             return redirect("/login")            
 
         user = users.first()        
@@ -68,13 +68,13 @@ def login():
         test_pw_hash, salt = make_pw_hash(password, salt).split(',')
 
         if test_pw_hash != pw_hash:
-            flash('Error: Incorrect password', category="error")
+            flash('Error: Incorrect password', category="is-warning")
             return redirect("/login")     
         
         # SUCCESS # 
 
         session['user'] = user.email
-        flash('Welcome Back, '+user.email, category="message")
+        flash('Welcome Back, '+user.email, category="is-primary")
         return redirect("/")
 
 
@@ -96,33 +96,33 @@ def register():
         # TODO: on failure, make sure email and username are passed back through to form fields
 
         if email == '' or username == '' or password == '' or verify == '':
-            flash('Error: One or more fields left blank', category="error")
+            flash('Error: One or more fields left blank', category="is-warning")
             return redirect('/register')
 
         if not is_email(email):
-            flash('Error: Entered email is not formatted like an email address', category="error")
+            flash('Error: Entered email is not formatted like an email address', category="is-warning")
             return redirect('/register')
 
         if not is_valid_username(username):
-            flash('Error: Usernames can only include: hyphens, underscores, letters and numbers', category="error")
+            flash('Error: Usernames can only include: hyphens, underscores, letters and numbers', category="is-warning")
             return redirect('/register')
         
         email_db_count = User.query.filter_by(email=email).count()
         if email_db_count > 0:
-            flash('Error: Email is already taken.', category="error")
+            flash('Error: Email is already taken.', category="is-warning")
             return redirect('/register')
 
         username_db_count = User.query.filter_by(username=username).count()
         if username_db_count > 0:
-            flash('Error: Username is already taken.', category="error")
+            flash('Error: Username is already taken.', category="is-warning")
             return redirect('/register')
 
         if len(email) < 3 or len(username) < 3 or len(password) < 3:
-            flash('Error: Email, username or password has fewer than three characters', category="error")
+            flash('Error: Email, username or password has fewer than three characters', category="is-warning")
             return redirect('/register')            
 
         if password != verify:
-            flash('Error: Passwords entered do not match.', category="error")
+            flash('Error: Passwords entered do not match.', category="is-warning")
             return redirect('/register')
 
         # SUCCESS #
@@ -133,7 +133,7 @@ def register():
         db.session.commit()
         
         session['user'] = user.email
-        flash('Successfully registered, '+user.email, category="message")
+        flash('Successfully registered, '+user.email, category="is-primary")
         return redirect("/")
 
 
@@ -141,7 +141,7 @@ def register():
 def logout():
     user = session['user']
     del session['user']
-    flash('Successfully logged out of '+user, category="message")
+    flash('Successfully logged out of '+user, category="is-primary")
     return redirect("/")
 
 
@@ -189,7 +189,7 @@ def blog(username=None):
         author = User.query.filter_by(username=username).first()
 
         if not author:
-            flash('Error: Requested username not in database.', category="error")
+            flash('Error: Requested username not in database.', category="is-warning")
             return redirect("/blog")
 
         postlist = db.session.query(Post, User).join(User).filter(User.username == username).all()
@@ -207,7 +207,7 @@ def blog_posts(url_post_id=None):
     if url_post_id:
         postlist = db.session.query(Post, User).join(User).filter(Post.id == url_post_id).all()
         if not postlist:
-            flash('Error: Requested post not in database.', category="error")
+            flash('Error: Requested post not in database.', category="is-warning")
             return redirect("/blog/posts")
         return render_template("blog.html", postlist=postlist)    
     
@@ -227,13 +227,13 @@ def newpost():
         body = request.form['body']
         
         if title == '' or body == '':
-            flash("Title and Body fields cannot be left blank", category="error")
+            flash("Title and Body fields cannot be left blank", category="is-warning")
             return render_template("newpost.html", title=title, body=body)
         
         new_post = Post(title, body, logged_in_user())
         db.session.add(new_post)
         db.session.commit()
-        flash("Success: New blog post published.", category="message")
+        flash("Success: New blog post published.", category="is-primary")
         return render_template('blog.html', postlist=db.session.query(Post, User).join(User).filter(Post.id == new_post.id).all())
 
 
@@ -258,13 +258,13 @@ def web_caesar():
         rot = request.form['rot']
         
         if text == '' or rot == '':
-            flash('Enter required values. Either rot or text area is blank.', category="error")
+            flash('Enter required values. Either rotation or plaintext area is blank.', category="is-warning")
             return render_template('web_caesar.html', text=text, rot=rot)
 
         try:
             text = caesar.encrypt(text, rot)
         except ValueError:
-            flash('Numbers only for rotation value.', category="error")
+            flash('Numbers only for rotation value.', category="is-warning")
 
     return render_template('web_caesar.html', text=text, rot=rot)
 
